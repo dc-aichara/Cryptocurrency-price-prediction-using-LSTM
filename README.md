d78270e7379d4280adebd075351e4e74d3805c99
Dayal Chand Aichara <dc.aichara@lastroots.com>
2019-05-31 12:16:21 +0900

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Cryptocurrency-price-prediction-using-LSTM
+This script can be used to predict price of cryptocurrency which is listed on CoinMarketCap. 
+
+## Example 
+I have tested this script for Bitcoin. 
+```
+$ python price_predict.py bitcoin 20130428 20190530
+
+```
+##  Results
+![](results/lstm_price_bitcoin.png)
--- price_predict.py
+++ price_predict.py
@@ -1,8 +1,8 @@
+import sys
 from PriceIndices import price
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
@@ -14,72 +14,86 @@ config = tf.ConfigProto(intra_op_parallelism_threads=0,
 
 session = tf.Session(config=config)
 
-
 price = price()
-df = price.get_price('bitcoin', '20130428', '20190529')
 
-df['date'] = pd.to_datetime(df['date'])
 
-df = df.sort_values(by ='date')
+def price_predict(coin, start_date, end_date):
 
-df = df.set_index(df['date'])[['price']]
+    print(coin, start_date, end_date)
+    df = price.get_price(str(coin), str(start_date), str(end_date))
 
-dataset = df.values
+    df['date'] = pd.to_datetime(df['date'])
 
-length = round(len(df)*0.80)
+    df = df.sort_values(by ='date')
 
-train = dataset[0:length, :]
-valid = dataset[length:, :]
+    df = df.set_index(df['date'])[['price']]
 
-# converting dataset into x_train and y_train
+    dataset = df.values
 
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(dataset)
+    length = round(len(df)*0.80)
 
-x_train, y_train = [], []
-for i in range(60,len(train)):
-    x_train.append(scaled_data[i-60:i, 0]) # Create len(train)-60 batches. Each batch has 60 values
-    y_train.append(scaled_data[i, 0])      # one batch of len(train)-60 values
-x_train, y_train = np.array(x_train), np.array(y_train)
+    train = dataset[0:length, :]
+    valid = dataset[length:, :]
 
-x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1], 1))
+    # converting dataset into x_train and y_train
 
-# create and fit the LSTM network
-model = Sequential()
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
-model.add(LSTM(units=50))
-model.add(Dense(1))
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(dataset)
 
-model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(x_train, y_train, epochs=5, batch_size=1, verbose=2)
+    x_train, y_train = [], []
+    for i in range(60,len(train)):
+        x_train.append(scaled_data[i-60:i, 0]) # Create len(train)-60 batches. Each batch has 60 values
+        y_train.append(scaled_data[i, 0])      # one batch of len(train)-60 values
+    x_train, y_train = np.array(x_train), np.array(y_train)
 
-# predicting length -len(df) values, using past 60 from the train data
-inputs = df[len(df) - len(valid) - 60:].values
-inputs = inputs.reshape(-1,1)
-inputs  = scaler.transform(inputs)
+    x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1], 1))
 
-X_test = []
-for i in range(60,inputs.shape[0]):
-    X_test.append(inputs[i-60:i,0]) # length -len(df) batches with each 60 values
-X_test = np.array(X_test)
+    # create and fit the LSTM network
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+    model.add(LSTM(units=50))
+    model.add(Dense(1))
 
-X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
-pred_price = model.predict(X_test)
-pred_price = scaler.inverse_transform(pred_price)
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(x_train, y_train, epochs=5, batch_size=1, verbose=2)
 
-rms=np.sqrt(np.mean(np.power((valid-pred_price),2)))
+    # predicting length -len(df) values, using past 60 from the train data
+    inputs = df[len(df) - len(valid) - 60:].values
+    inputs = inputs.reshape(-1,1)
+    inputs  = scaler.transform(inputs)
 
-print(rms)
+    X_test = []
+    for i in range(60,inputs.shape[0]):
+        X_test.append(inputs[i-60:i,0]) # length -len(df) batches with each 60 values
+    X_test = np.array(X_test)
 
-# for plotting
+    X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
+    pred_price = model.predict(X_test)
+    pred_price = scaler.inverse_transform(pred_price)
 
-plt.figure(figsize =(16, 12))
-train = df[:length]
-valid = df[length:]
-valid['Predictions'] = pred_price
-plt.plot(train['price'])
-plt.plot(valid[['price']])
-plt.plot(valid[['Predictions']], color = 'r')
+    rms=np.sqrt(np.mean(np.power((valid-pred_price),2)))
 
-plt.show()
+    print(rms)
+
+    # for plotting
+
+    fig, ax = plt.subplots(figsize =(16, 12))
+    train = df[:length]
+    valid = df[length:]
+    valid['Predictions'] = pred_price
+    plt.plot(train['price'], label = 'Price')
+    plt.plot(valid[['price']], label = 'Actual Price')
+    plt.plot(valid[['Predictions']], color = 'r', label = 'Predicted Price')
+    plt.xlabel('Time', color = 'b', fontsize = 24)
+    plt.ylabel('Price ($)', color ='b', fontsize =24)
+    plt.title('Price Prediction using LSTM', color ='b', fontsize =27)
+    plt.grid()
+    fig.set_facecolor('orange')
+    plt.legend(fontsize =24)
+    plt.savefig('results/lstm_price_{}.png'.format(str(coin)), bbox_inches ='tight', facecolor ='orange')
+    plt.show()
+
+
+if __name__ == '__main__':
+    price_predict(sys.argv[1], sys.argv[2], sys.argv[3])
 
